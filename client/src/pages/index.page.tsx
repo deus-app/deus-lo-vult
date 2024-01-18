d946b48c808764e51d5848ba9df02f5bebe4ed61
s1f102100793 <s1f102100793@iniad.org>
2024-01-18 22:13:33 +0900

    fix: styling index.page.tsx

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -11,86 +11,99 @@ import {
   Message,
   MessageList,
 } from '@chatscope/chat-ui-kit-react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { NameLabel } from './@components/NameLabel/NameLabel';
 import { StatusIcon } from './@components/StatusIcon/StatusIcon';
 import styles from './index.module.css';
 
-const Home = (service: ServiceModel) => {
+const Home = () => {
   const [services, setServices] = useState<ServiceModel[]>([]);
+  const [currentServiceIndex, setCurrentServiceIndex] = useState(0);
+  const service = services[currentServiceIndex];
 
   const fetchServices = async () => {
     const services = await apiClient.apps.$get().catch(returnNull);
 
-    if (services !== null) setServices(services);
+    if (services !== null) {
+      setServices(services);
+    }
   };
+
+  const changeService = (direction: string) => {
+    setCurrentServiceIndex((prevIndex) => {
+      const newIndex =
+        direction === 'next'
+          ? (prevIndex + 1) % services.length
+          : (prevIndex - 1 + services.length) % services.length;
+
+      return newIndex;
+    });
+  };
+
+  useEffect(() => {
+    fetchServices();
+  }, []);
 
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <StatusIcon status={service.status} />
-        <Spacer axis="x" size={12} />
-        <span className={styles.indexLabel}>No.{service.id}</span>
+        {service && (
+          <>
+            <StatusIcon status={service.status} />
+            <Spacer axis="x" size={12} />
+            <button onClick={() => changeService('prev')}>&lt;</button>
+            <Spacer axis="x" size={12} />
+            <span className={styles.indexLabel}>No.{currentServiceIndex}</span>
+            <button onClick={() => changeService('next')}>&gt;</button>
+          </>
+        )}
       </div>
-      <div style={{ flex: 1, position: 'relative' }}>
-        <MainContainer className={styles.mainContainer}>
-          <ChatContainer style={{ background: 'transparent' }}>
-            <MessageList
-              style={{ background: 'transparent' }}
-              // typingIndicator={
-              //   props.app.status === 'running' &&
-              //   isClosed && (
-              //     <TypingIndicator
-              //       style={{ background: 'transparent' }}
-              //       content="ChatGPTが思考中"
-              //     />
-              //   )
-              // }
-            >
-              {service.ideas.map((idea) => {
-                return (
-                  <>
+      <MainContainer className={styles.mainContainer}>
+        <ChatContainer style={{ background: 'transparent' }}>
+          <MessageList style={{ background: 'transparent' }}>
+            {service.ideas.map((idea) => {
+              return (
+                <>
+                  <Message
+                    key={idea.id}
+                    model={{ type: 'custom' } as MessageModel}
+                    avatarPosition="tl"
+                  >
+                    <Avatar>
+                      <Spacer axis="y" size={26} />
+                      <Spacer axis="x" size={4} />
+                      <ChatGPTIcon size={32} fill="balck" />
+                    </Avatar>
+                    <Message.CustomContent>
+                      <NameLabel name="GPT4-turbo" createdTime={idea.createdAt} />
+                      <Spacer axis="y" size={6} />
+                      <div>{idea.description}</div>
+                    </Message.CustomContent>
+                  </Message>
+                  {idea.feedback && (
                     <Message
                       key={idea.id}
                       model={{ type: 'custom' } as MessageModel}
-                      avatarPosition="tl"
+                      avatarPosition="tr"
                     >
                       <Avatar>
                         <Spacer axis="y" size={26} />
                         <Spacer axis="x" size={4} />
-                        <ChatGPTIcon size={32} fill="#fff" />
+                        <ChatGPTIcon size={32} fill="#aaa" />
                       </Avatar>
                       <Message.CustomContent>
-                        <NameLabel name="GPT4-turbo" createdTime={idea.createdAt} />
+                        <NameLabel name="GPT4-turbo" createdTime={idea.feedback.createdAt} />
                         <Spacer axis="y" size={6} />
-                        <div>{idea.description}</div>
+                        <div>{idea.feedback.feedback}</div>
                       </Message.CustomContent>
                     </Message>
-                    {idea.feedback && (
-                      <Message
-                        key={idea.id}
-                        model={{ type: 'custom' } as MessageModel}
-                        avatarPosition="tr"
-                      >
-                        <Avatar>
-                          <Spacer axis="y" size={26} />
-                          <Spacer axis="x" size={4} />
-                          <ChatGPTIcon size={32} fill="#fff" />
-                        </Avatar>
-                        <Message.CustomContent>
-                          <NameLabel name="GPT4-turbo" createdTime={idea.feedback.createdAt} />
-                          <Spacer axis="y" size={6} />
-                          <div>{idea.feedback.feedback}</div>
-                        </Message.CustomContent>
-                      </Message>
-                    )}
-                  </>
-                );
-              })}
-            </MessageList>
-          </ChatContainer>
-        </MainContainer>
-      </div>
+                  )}
+                </>
+              );
+            })}
+          </MessageList>
+        </ChatContainer>
+      </MainContainer>
     </div>
   );
 };
