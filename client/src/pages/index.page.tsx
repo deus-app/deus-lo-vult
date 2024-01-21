94d3bcfd54abc04bb2f3ef2d01d23958e58c2188
s1f102100793 <s1f102100793@iniad.org>
2024-01-21 15:33:35 +0900

    fix: changeService function

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -11,7 +11,7 @@ import {
   Message,
   MessageList,
 } from '@chatscope/chat-ui-kit-react';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { NameLabel } from './@components/NameLabel/NameLabel';
 import { StatusIcon } from './@components/StatusIcon/StatusIcon';
 import styles from './index.module.css';
@@ -19,23 +19,40 @@ import styles from './index.module.css';
 // eslint-disable-next-line complexity
 const Home = () => {
   const [services, setServices] = useState<ServiceModel[]>([]);
-  const [currentServiceIndex, setCurrentServiceIndex] = useState(0);
-  const service = services[currentServiceIndex];
+  const [currentServiceId, setCurrentServiceId] = useState<number>();
+  const sortedSeedServices = useMemo(
+    () => services.sort((a, b) => a.createdAt - b.createdAt),
+    [services]
+  );
 
-  const fetchServices = async () => {
-    const services = await apiClient.apps.$get().catch(returnNull);
-
-    if (services !== null) {
-      setServices(services);
+  const currentService = useMemo(() => {
+    if (currentServiceId === undefined) return undefined;
+    const index = currentServiceId - 1;
+    if (index >= 0 && index < sortedSeedServices.length) {
+      return sortedSeedServices[index];
     }
-  };
+    return undefined;
+  }, [sortedSeedServices, currentServiceId]);
 
+  const fetchServices = useCallback(async () => {
+    const resuktServices = await apiClient.apps.$get().catch(returnNull);
+
+    if (resuktServices === null) return;
+    setServices(resuktServices);
+
+    if (currentServiceId === undefined) {
+      setCurrentServiceId(resuktServices.length);
+    }
+  }, [currentServiceId]);
+
+  // eslint-disable-next-line complexity
   const changeService = (direction: string) => {
-    setCurrentServiceIndex((prevIndex) => {
-      const newIndex = direction === 'next' ? prevIndex + 1 : prevIndex - 1;
-
-      return newIndex;
-    });
+    if (currentServiceId === undefined) return;
+    if (direction === 'next' && currentServiceId < services.length) {
+      setCurrentServiceId(currentServiceId + 1);
+    } else if (direction === 'prev' && currentServiceId > 1) {
+      setCurrentServiceId(currentServiceId - 1);
+    }
   };
 
   useEffect(() => {
@@ -44,18 +61,18 @@ const Home = () => {
     const intervalId = window.setInterval(fetchServices, 5_000);
 
     return () => clearInterval(intervalId);
-  }, []);
+  }, [fetchServices]);
 
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        {service && (
+        {currentService && (
           <>
-            <StatusIcon status={service.status} />
+            <StatusIcon status={currentService.status} />
             <Spacer axis="x" size={12} />
             <button onClick={() => changeService('prev')}>&lt;</button>
             <Spacer axis="x" size={12} />
-            <span className={styles.indexLabel}>No.{currentServiceIndex}</span>
+            <span className={styles.indexLabel}>No.{currentServiceId}</span>
             <button onClick={() => changeService('next')}>&gt;</button>
           </>
         )}
@@ -64,7 +81,7 @@ const Home = () => {
         <MainContainer className={styles.mainContainer}>
           <ChatContainer style={{ background: 'transparent' }}>
             <MessageList style={{ background: 'transparent' }}>
-              {service && (
+              {currentService && (
                 <Message model={{ type: 'custom' } as MessageModel} avatarPosition="tl">
                   <Avatar>
                     <Spacer axis="y" size={26} />
@@ -72,15 +89,15 @@ const Home = () => {
                     <ChatGPTIcon size={32} fill="#fff" />
                   </Avatar>
                   <Message.CustomContent>
-                    <NameLabel name="GPT4-turbo" createdTime={service.createdAt} />
+                    <NameLabel name="GPT4-turbo" createdTime={currentService.createdAt} />
                     <Spacer axis="y" size={6} />
                     <div>サービス領域：</div>
-                    <div>{service.area}</div>
+                    <div>{currentService.area}</div>
                   </Message.CustomContent>
                 </Message>
               )}
-              {service &&
-                service.ideas.map((idea) => [
+              {currentService &&
+                currentService.ideas.map((idea) => [
                   <Message
                     key={idea.id}
                     model={{ type: 'custom' } as MessageModel}
@@ -116,7 +133,7 @@ const Home = () => {
                     </Message>
                   ),
                 ])}
-              {service && service.name && service.similarName && (
+              {currentService && currentService.name && currentService.similarName && (
                 <Message model={{ type: 'custom' } as MessageModel} avatarPosition="tl">
                   <Avatar>
                     <Spacer axis="y" size={26} />
@@ -124,10 +141,10 @@ const Home = () => {
                     <ChatGPTIcon size={32} fill="#fff" />
                   </Avatar>
                   <Message.CustomContent>
-                    <NameLabel name="GPT4-turbo" createdTime={service.createdAt} />
+                    <NameLabel name="GPT4-turbo" createdTime={currentService.createdAt} />
                     <Spacer axis="y" size={6} />
-                    <div>アプリ名: {service.name}</div>
-                    <div>類似サービス:{service.similarName}</div>
+                    <div>アプリ名: {currentService.name}</div>
+                    <div>類似サービス:{currentService.similarName}</div>
                   </Message.CustomContent>
                 </Message>
               )}
